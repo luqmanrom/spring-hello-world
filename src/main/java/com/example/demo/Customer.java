508f58a1fb9f86738e35a49a91b4c774b3a0fba2
Luqman Hakim Mohamad Rom <luqmanrom@gmail.com>
2020-10-01 00:11:12 +0800

    Install Lombok to avoid getter and setter

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class Customer {
 
     @Id
@@ -14,30 +18,5 @@ public class Customer {
 
     private String firstName;
     private String lastName;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
 
 }
