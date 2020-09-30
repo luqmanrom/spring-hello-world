8132b64f714015abb6541e7cbcc8de35cca452f1
Luqman Hakim Mohamad Rom <luqmanrom@gmail.com>
2020-09-30 22:52:27 +0800

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+https://github.com/thymeleaf
+
+
+ORM Hibernate
+
+// TODO
+
+1. Figure out how to generate getter and setter
+2. 
---
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    Customer findCustomerById(Integer id);
+
+}
